beea0e3379de59aa2e8bde98a67b196bfe88f34a
fanyuchen <fanyuchen@Mac02.local>
2016-03-21 21:22:02 +0800

    设置计算机洁面

---
+++ ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ppp
+//
+//  Created by 范宇辰 on 16/3/17.
+//  Copyright © 2016年 范宇辰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var no1: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func no1(sender: AnyObject) {
+            print("hehe")
+    }
+}
+
